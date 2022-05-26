4361bd86b912cb1f39dc32a7c2c4eb14aa965060
aghimire <ashim.ghimire@piceasoft.com>
2022-05-26 22:14:42 +0300

    success network request, and response

--- app/src/main/java/com/example/networktestproject/MainActivity.kt
+++ app/src/main/java/com/example/networktestproject/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.networktestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.networktestproject.databinding.ActivityMainBinding
+import retrofit2.HttpException
+import java.io.IOException
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var todoAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpRecyclerView()
+        lifecycleScope.launchWhenCreated {
+            binding.mainProgressBar.isVisible = true
+
+            val response= try {
+                RetrofitInstance.api.getTodos()
+            }catch (e: IOException){
+                Log.d(TAG, "IOException")
+                return@launchWhenCreated
+            } catch (e: HttpException){
+                Log.d(TAG, "Http exception")
+                return@launchWhenCreated
+            }
+            if (response.isSuccessful && response.body() != null){
+                todoAdapter.todos = response.body()!!
+            }else{
+                Log.d(TAG, "Response not successful")
+            }
+            binding.mainProgressBar.isVisible = false
+        }
+
+
+    }
+
+    private fun setUpRecyclerView() = binding.rvTodos.apply {
+        todoAdapter = TodoAdapter()
+        adapter= todoAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
